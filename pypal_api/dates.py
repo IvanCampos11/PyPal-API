d4f05204b4d2f3331e1a06eda8fe151d6379d261
Ivan Campos <ivancampos11@protonmail.com>
2021-12-01 16:58:38 -0500

    Finished entirely

--- pypal_api/dates.py
+++ pypal_api/dates.py
@@ -1,3 +1,109 @@
+from pypal_api.errors import InvalidInputError
+from datetime import timedelta
+from datetime import timedelta, date
+import sys
+from pathlib import Path
+
+file = Path(__file__). resolve()
+package_root_directory = file.parents[1]
+sys.path.append(str(package_root_directory))
+
+
 def date_to_string(date_input):
+    """
+    Input a datetime object and it'll return in a nice and neat string
+
+    ----------
+    Parameters
+    ----------
+    date_input: Datetime
+
+    -------
+    Returns
+    -------
+    String
+    """
     new_date = str(date_input).split(" ")
     return new_date[0]
+
+
+def new_date(num_days, from_date=date.today(), return_type='string', weekends=True, weekdays=True):
+    """
+    Enter the number of days either ahead or behind, the returned data will be the correct date
+    in the specified format (default str)
+
+    ----------
+    Parameters
+    ----------
+    num_days : Integer
+    from_date : Date, String, or Integer
+    type : Date, String, or Integer
+    weekends : Boolean
+    weekdays : Boolean
+
+    -------
+    Returns
+    -------
+    String
+    """
+    if type(num_days) != int:
+        error_message = f"""
+        '{num_days}' isn't a valid input! Required (Integer)
+        """
+        raise InvalidInputError(error_message)
+    if type(weekends) != bool:
+        error_message = f"""
+        '{num_days}' isn't a valid input! Required (Boolean)
+        """
+        raise InvalidInputError(error_message)
+    if type(weekdays) != bool:
+        error_message = f"""
+        '{num_days}' isn't a valid input! Required (Boolean)
+        """
+        raise InvalidInputError(error_message)
+    if type(return_type) != str:
+        error_message = f"""
+        '{num_days}' isn't a valid input! Required (String)
+        """
+        raise InvalidInputError(error_message)
+
+    types = {
+        "string": ['str', 'string'],
+        "date": ['date', 'datetime', 'date time'],
+        "integer list": ['int list', 'integer list', 'int-list', 'integer-list', 'int-lst', 'integer-lst'],
+        "string list": ['str list', 'string list', 'str-list', 'string-list', 'str-lst', 'string-lst'],
+        'list': ['lst', 'list']
+    }
+    if str(num_days).startswith('-'):
+        num_days = int(str(num_days)[1:])
+        new_created_date = from_date - timedelta(days=num_days)
+    else:
+        new_created_date = from_date + timedelta(days=num_days)
+
+    for key, value in types.items():
+        if return_type.lower() in value:
+            edited_return_type = key
+            break
+        else:
+            edited_return_type = ''
+
+    if edited_return_type == '':
+        error_message = f"""
+        "{return_type}" cannot be used as a type. Options (integer-list, string-list, string, date)
+        """
+        raise InvalidInputError(error_message)
+
+    elif edited_return_type == 'string':
+        return str(new_created_date)
+    elif edited_return_type == 'date':
+        return new_created_date
+    elif edited_return_type == 'integer list' or edited_return_type == 'list':
+        string_list = str(new_created_date).split("-")
+        new_created_date = []
+        for number in string_list:
+            if number.startswith('0'):
+                number = number[1:]
+            new_created_date.append(int(number))
+        return new_created_date
+    elif edited_return_type == 'string list':
+        return str(new_created_date).split("-")
--- tests/test_dates.py
+++ tests/test_dates.py
@@ -1,21 +1,21 @@
-import os, sys
-
-import sys  
+import os
+import sys
 from pathlib import Path
-file = Path(__file__). resolve()  
-package_root_directory = file.parents[1]
-sys.path.append(str(package_root_directory))
 import datetime
 import unittest
-from pypal_api.dates import date_to_string
+
+file = Path(__file__). resolve()
+package_root_directory = file.parents[1]
+sys.path.append(str(package_root_directory))
+from pypal_api.dates import date_to_string, new_date
 
 
 class DatesTest(unittest.TestCase):
     """These test if the dates functions work correctly"""
 
-    def test_dates(self):
-        date_one = datetime.datetime(2021, 12,1)
-        date_two = datetime.datetime(2021,11,29)
+    def test_date_to_string(self):
+        date_one = datetime.datetime(2021, 12, 1)
+        date_two = datetime.datetime(2021, 11, 29)
 
         finished_date_one = date_to_string(date_one)
         finished_date_two = date_to_string(date_two)
@@ -25,6 +25,21 @@ class DatesTest(unittest.TestCase):
 
         self.assertEqual(finished_date_two, "2021-11-29")
         self.assertEqual(type(finished_date_two), str)
+
+    def test_new_date(self):
+        test_day = 2
+        output_one = "string"
+        output_two = 'date'
+        date_from = datetime.datetime(2021, 12, 1)
+
+        test_one = new_date(test_day)
+        answer_one = datetime.date.today() + datetime.timedelta(days=2)
+        self.assertEqual(test_one, str(answer_one))
+
+        test_two = new_date(test_day, return_type=output_two)
+        answer_two = datetime.date.today() + datetime.timedelta(days=test_day)
+        self.assertEqual(test_two, answer_two)
+
 
 if __name__ == "__main__":
     unittest.main()
