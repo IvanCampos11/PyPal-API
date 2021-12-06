dea067f81f30c5013a52b1f5b5bb9de98ad10f5c
Ivan Campos <ivancampos11@protonmail.com>
2021-12-06 14:27:13 -0500

    Update pypal_pandas.py

--- pypal_api/pypal_pandas.py
+++ pypal_api/pypal_pandas.py
@@ -5,7 +5,20 @@ from pathlib import Path
 sys.path.append('')
 from pypal_api.exceptions import *
 
-def list_to_column(dateframes, lists, name):
+def listToColumn(dateframes, lists, name):
     newSeries = pd.Series(lists)
     dateframes[name] = newSeries
     return dateframes
+
+def nullReport(dataframe):
+    nullFound = dataframe.isnull().sum().sum()
+    if (nullFound == 1):
+        answer1 = print('There is only', nullFound,
+                        'NaN value in your DataFrame!')
+        return answer1
+    elif (nullFound > 1):
+        answer2 = print('There are', nullFound,
+                        'NaN values in your DataFrame.')
+        return answer2
+    else:
+        print('There are no NaN values in your DataFrame!')
