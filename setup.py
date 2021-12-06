c274c7bad563d3a35ef814e7372f693f7db8b2c5
Ivan Campos <ivancampos11@protonmail.com>
2021-12-06 09:54:55 -0500

    update to 0.1.2, fixed inability to install python package

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 import setuptools
 REQUIRED = [
-    "numpy",
-    "pandas",
-    "ShopifyAPI",
-    "fastapi",
-    "gspread",
-    "oauth2client",
-    "monday",
-    "requests",
-    "python-dotenv"
+    "numpy>=1.21.4"
+    "pandas>=1.3.4"
+    "ShopifyAPI>=9.0.0"
+    "requests"
+    "fastapi>=0.70.0"
+    "gspread>=5.0.0"
+    "oauth2client>=4.1.3"
+    "monday>=1.2.7"
+    "python-dotenv>=0.19.2"
 ]
 with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
 setuptools.setup(
     name="pypal-api",
-    version="0.1.1",
+    version="0.1.2",
     author="Ivan Campos",
     description="A collection of helper functions to ease API development",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/IvanCampos11/PyPal-API",
-    packages=setuptools.find_packages(),
+    packages=[ 'pypal_api' ],
     python_requires=">=3.8",
     install_requires=REQUIRED,
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
