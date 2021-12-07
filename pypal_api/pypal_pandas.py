4268ecf4dbfb4f55916d362223cdebdccf0d2033
Ivan Campos <ivancampos11@protonmail.com>
2021-12-07 15:51:00 -0500

    Update pypal_pandas.py

--- pypal_api/pypal_pandas.py
+++ pypal_api/pypal_pandas.py
@@ -27,44 +27,41 @@ def listToColumn(dateframes, lists, name):
     dateframes[name] = newSeries
     return dateframes
 
-class Report:
-    def __init__(self, df):
-        self.dataframe = df
 
-    def nullReport(self):
-        """
-        Get a detailed report on the health of the dataframe given.
+def nullReport(self):
+    """
+    Get a detailed report on the health of the dataframe given.
 
-        ----------
-        Parameters
-        ----------
-        dataframe : pandas.Dataframe
+    ----------
+    Parameters
+    ----------
+    dataframe : pandas.Dataframe
 
-        -------
-        Returns
-        -------
-        Strings
-        """
-        nullFound = self.dataframe.isnull().sum().sum()
-        if (nullFound == 1):
-            answer1 = print('There is only', nullFound,
-                            'NaN value in your DataFrame!')
-            return answer1
-        elif (nullFound > 1):
-            answer2 = print('There are', nullFound,
-                            'NaN values in your DataFrame.')
-            return answer2
-        else:
-            print('There are no NaN values in your DataFrame!')
+    -------
+    Returns
+    -------
+    Strings
+    """
+    nullFound = self.dataframe.isnull().sum().sum()
+    if (nullFound == 1):
+        answer1 = print('There is only', nullFound,
+                        'NaN value in your DataFrame!')
+        return answer1
+    elif (nullFound > 1):
+        answer2 = print('There are', nullFound,
+                        'NaN values in your DataFrame.')
+        return answer2
+    else:
+        print('There are no NaN values in your DataFrame!')
+
+def nullClean(self):
+    self.dataFrame = self.dataFrame.replace({
+        '?': np.nan, 'nan': np.nan, 'Nan': np.nan, 'n/a': np.nan,
+        'NaN': np.nan, ' ?': np.nan, 'N/A': np.nan, 'na': np.nan, 'NAN': np.nan
+    })
+    nullValue = self.dataFrame.isnull().sum().sum()
+    self.dataFrame.dropna(inplace=True)
+    print('There were', nullValue, "NaN's detected and removed.")
+    return self.dataFrame
     
-    def nullClean(self):
-        self.dataFrame = self.dataFrame.replace({
-            '?': np.nan, 'nan': np.nan, 'Nan': np.nan, 'n/a': np.nan,
-            'NaN': np.nan, ' ?': np.nan, 'N/A': np.nan, 'na': np.nan, 'NAN': np.nan
-        })
-        nullValue = self.dataFrame.isnull().sum().sum()
-        self.dataFrame.dropna(inplace=True)
-        print('There were', nullValue, "NaN's detected and removed.")
-        return self.dataFrame
-        
-        
+    
